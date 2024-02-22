26191480448d6bec6a1aa26c25e72fe13bc3a286
RoeYeoR <157602723+RoeYeoR@users.noreply.github.com>
2024-02-22 22:50:44 +0200

    Add files via upload

--- Main.c
+++ Main.c
@@ -24,20 +24,20 @@ void displayMenu() {
 }
 
 // Function to insert strings into the list
-void insertStrings(StrList** list) {
+void insertStrings(StrList* list) {
     int numWords;
-    printf("Enter the number of words to insert: ");
+    //printf("Enter the number of words to insert: ");
     scanf("%d", &numWords);
 
     char buffer[100]; // Assuming maximum word length is 99 characters
     fgets(buffer, sizeof(buffer), stdin); // Clear input buffer
 
-    printf("Enter the words separated by space:\n");
+    //printf("Enter the words separated by space:\n");
     fgets(buffer, sizeof(buffer), stdin);
 
     char* token = strtok(buffer, " ");
     while (token != NULL) {
-        StrList_insertLast(&list, token);
+        StrList_insertLast(list, token);
         token = strtok(NULL, " ");
     }
 }
@@ -49,76 +49,76 @@ int main() {
     int index;
 
     do {
-        displayMenu();
+        //displayMenu();
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                insertStrings(&list);
+                insertStrings(list);
                 break;
             case 2:
-                printf("Enter the index: ");
+                //printf("Enter the index: ");
                 scanf("%d", &index);
-                printf("Enter the string: ");
+                //printf("Enter the string: ");
                 scanf("%s", buffer);
-                StrList_insertAt(&list, buffer, index);
+                StrList_insertAt(list, buffer, index);
                 break;
             case 3:
-                printf("List: ");
+                //printf("List: ");
                 StrList_print(list);
                 break;
             case 4:
-                printf("Length of the list: %zu\n", StrList_size(list));
+                StrList_size(list);
                 break;
             case 5:
-                printf("Enter the index: ");
+                //printf("Enter the index: ");
                 scanf("%d", &index);
-                printf("String at index %d: ", index);
+                //printf("String at index %d: ", index);
                 StrList_printAt(list, index);
                 break;
             case 6:
-                printf("Total characters in the list: %d\n", StrList_printLen(list));
+                StrList_printLen(list);
                 break;
             case 7:
-                printf("Enter the string: ");
+                //printf("Enter the string: ");
                 scanf("%s", buffer);
-                printf("Number of times '%s' appears: %d\n", buffer, StrList_count(list, buffer));
+                StrList_count(list, buffer);
                 break;
             case 8:
-                printf("Enter the string to delete: ");
+                //printf("Enter the string to delete: ");
                 scanf("%s", buffer);
-                StrList_remove(&list, buffer);
+                StrList_remove(list, buffer);
                 break;
             case 9:
-                printf("Enter the index to delete: ");
+                //printf("Enter the index to delete: ");
                 scanf("%d", &index);
-                StrList_removeAt(&list, index);
+                StrList_removeAt(list, index);
                 break;
             case 10:
-                StrList_reverse(&list);
-                printf("List reversed.\n");
+                StrList_reverse(list);
+                //printf("List reversed.\n");
                 break;
             case 11:
                 StrList_free(list);
                 list = StrList_alloc();
-                printf("List deleted.\n");
+                //printf("List deleted.\n");
                 break;
             case 12:
                 StrList_sort(list);
-                printf("List sorted in lexicographical order.\n");
+                //printf("List sorted in lexicographical order.\n");
                 break;
             case 13:
                 if (StrList_isSorted(list)) {
-                    printf("List is arranged in lexicographical order.\n");
+                    printf("True\n");
                 } else {
-                    printf("List is not arranged in lexicographical order.\n");
+                    printf("False\n");
                 }
                 break;
             case 0:
-                printf("Exiting program.\n");
+                //printf("Exiting program.\n");
                 break;
             default:
-                printf("Invalid choice. Please try again.\n");
+                //printf("Invalid choice. Please try again.\n");
                 break;
         }
     } while (choice != 0);
